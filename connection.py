d614290b93a4177c41df7cc9c63495c2ac34d58a
tahamoeini <taha.moeini@gmail.com>
2022-03-13 21:29:32 +0000

    rounding hours and calculate based on maxDuration

--- connection.py
+++ connection.py
@@ -1,59 +1,81 @@
 import requests
+import math
 from pick import pick
 import pytz
 import urllib
 import datetime
-from secrets import key
+import secrets
 
 api = 'https://api.track.toggl.com/api/'
 apiVersion = 'v8'
 apiUrl = api+apiVersion+'/'
-
-# authResponse = requests.get(apiUrl+'me?with_related_data=true', auth=(key,'api_token')).json()
-# data = authResponse['data']
-# authResponseFast = requests.get(apiUrl+'me', auth=(key,'api_token')).json()
-# dataFast = authResponseFast['data']
-
 
 endDate = datetime.datetime.now(pytz.utc)
 startDate = endDate - datetime.timedelta(days=7)
 endDate = str(endDate.replace(microsecond=0).isoformat())
 startDate = str(startDate.replace(microsecond=0).isoformat())
 
-workspaces = requests.get(apiUrl+'workspaces', auth=(key, 'api_token')).json()
+if secrets.singleWorkSpace:
+    workspaceId = secrets.singleWorkSpace
+else:
+    workspaces = requests.get(apiUrl+'workspaces', auth=(secrets.key, 'api_token')).json()
 
-print(workspaces)
+    workspacesList = {}
+    for workspace in workspaces:
+        workspacesList.update({
+            workspace['id']: workspace['name'],
+        })
 
-workspacesList = {}
-for workspace in workspaces:
-    workspacesList[workspace['id']] = workspace['name']
+    workspacesOptions = workspacesList.values()
+    workspacesIds = workspacesList.keys()
 
-workspacesOptions = workspacesList.values()
-workspacesIds = workspacesList.keys()
+    workspace, index = pick(list(workspacesOptions), "Select a Workspace:")
 
-print(workspacesOptions)
-print(workspacesIds)
+    position = list(workspacesOptions).index(workspace)
 
-workspace, index = pick(list(workspacesOptions), "Select a Workspace:")
+    workspaceId = list(workspacesIds)[position]
 
-position = list(workspacesOptions).index(workspace)
 
-workspaceId = list(workspacesIds)[position]
+class Project:
+    def __init__(self, id, name, duration):
+        self.id = id
+        self.name = name
+        self.duration = duration
+
 
 projects = requests.get(apiUrl+'workspaces/' + str(workspaceId) + '/projects',
-                        auth=(key, 'api_token')).json()
+                        auth=(secrets.key, 'api_token')).json()
 
 projectsList = {}
+
 for project in projects:
-    projectsList[project['id']] = project['name']
+    project = Project(project['id'], project['name'], 0)
+    projectsList.update({
+        project.id: project,
+    })
 
-projectsOptions = projectsList.values()
-projectsIds = projectsList.keys()
+timeEntries = requests.get(apiUrl+urllib.parse.quote('time_entries?start_date=' +
+                           startDate+'&end_date='+endDate, safe='=?&'), auth=(secrets.key, 'api_token')).json()
 
-project, index = pick(list(projectsOptions), "Select a Project:")
+for timeEntry in timeEntries:
+    projectId = timeEntry['pid']
+    project = projectsList[projectId]
+    project.duration += timeEntry['duration']
+    projectsList.update({
+        project.id: project,
+    })
 
-position = list(projectsOptions).index(project)
-
-projectId = list(projectsIds)[position]
-
-# timeEntries = requests.get(apiUrl+urllib.parse.quote('time_entries?start_date='+startDate+'&end_date='+endDate,safe='=?&'), auth=(key,'api_token')).json()
+if secrets.maxDurationOfProjects:
+    for project in projectsList.values():
+        try:
+            secrets.maxDurationOfProjects[project.name]
+        except:
+            secrets.maxDurationOfProjects.update({
+                project.name: 0,
+            })
+        if secrets.maxDurationOfProjects[project.name] < project.duration/3600:
+            print(project.name+': '+str(math.ceil(project.duration/360)/10)+'h - OverDo! with ' +
+                  str(math.ceil((project.duration/3600 - secrets.maxDurationOfProjects[project.name])*10)/10)+'h exceeded')
+        else:
+            print(project.name+': ' +
+                  str(math.ceil(project.duration/360)/10)+'h - Good...')
